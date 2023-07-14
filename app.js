74e4bfb65c3213ec0097eb44035226989de8d744
Eric Mahare <erikmahare@gmail.com>
2023-07-14 08:04:56 +0300

    Add the necessary middlewares

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-const path = require("path");
+require('dotenv').config({path: './.env'})
 const express = require('express');
-const ejs = require('ejs')
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload')
+const blockRoutes = require('./routes/blog')
+const dbConnect = require('./db/dbConnect')
+const validateMiddleWare = require('./middlewares/validateMiddleWare')
 // Listen to server request
 const app = express()
 // middlewares
 app.use(express.static('./public'))
 app.set('view engine', 'ejs')
-// Home page
-app.get('/',(req, res) => {
-  res.status(200).render('index')
-})
-
-// About page
-app.get('/about', (req, res) => {
-  res.status(200).render('about')
-})
-
-// contact page
-app.get('/contact', (req, res) => {
-  res.status(200).render('contact')
-})
-
-// post route 
-app.get('/post', (req, res) => {
-  res.status(200).render('post')
-})
-
-// 404 page
-app.get('*',(req, res) => {
-  res.status(404).render('404')
-})
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(blockRoutes)
+app.use("/posts/new", validateMiddleWare)
+app.use(fileUpload())
 // start server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
-app.listen(PORT, console.log(`Listening to port ${PORT}`))
+const start = async () => {
+  try {
+    await dbConnect
+    app.listen(PORT, console.log(`Listening to port ${PORT}`))
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+start()
